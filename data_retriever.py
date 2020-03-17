01645f5472903be3f07f939c5dfeb6a30b77dc2e
“[firstname <karolos.koutsoulelos@gmail.com>
2020-03-17 17:23:47 +0200

    Cleaned up service file

--- data_retriever.py
+++ data_retriever.py
@@ -20,31 +20,6 @@ def get_all_data():
 def add_thousand_separator(value):
     return format(value, ',d')
 
-def get_all_country_data():
-    response = requests.get('https://corona.lmao.ninja/countries')
-    response_to_list = json.loads(response.text)
-
-    countries = []
-    deaths = []
-
-    for item in response_to_list:
-        if item.get("deaths") > 0:
-            countries.append(item.get("country"))
-            deaths.append(item.get("deaths"))
-    return[countries, deaths]
-
-
-def get_all_country_data_as_json():
-    response = requests.get('https://corona.lmao.ninja/countries')
-    return json.loads(response.text)
-  
-def get_data_keys():
-    response = requests.get('https://corona.lmao.ninja/countries/greece')
-    return([i for i in json.loads(response.text).keys()])
-
-def get_data_per_country():
-    pass
-    
 def create_data_file():
     filename = 'coronavirus_data_file_' + str(date.today()) + '.csv'
     
