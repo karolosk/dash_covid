dd853acec1e44ddfe292ccc5293c31302b31608f
“[firstname <karolos.koutsoulelos@gmail.com>
2020-03-17 16:59:04 +0200

    Removed callbacks and pagination from table

--- app.py
+++ app.py
@@ -65,73 +65,37 @@ app.layout = html.Div([
         ]
     ),
 
-      
-    html.H4("Country Data"),
-    dash_table.DataTable(
-        id='global-data-table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-        filter_action="native",
-        sort_action="native",
-        sort_mode="multi",
-        page_size= 10,
-        row_selectable='multi',
-        style_header={
-            'fontWeight': 'bold'
-        },
-        # Zebra style
-        style_data_conditional=[{
-            'if': {'row_index': 'odd'},
-            'backgroundColor': 'rgb(248, 248, 248)'
-        }],
-        
-        
-    ),
-    html.Div(id='datatable-interactivity-container'),
-])
-
-@app.callback(
-    Output('datatable-interactivity-container', "children"),
-    [Input('global-data-table', "derived_virtual_data"),
-     Input('global-data-table', "derived_virtual_selected_rows")])
-def update_graphs(rows, derived_virtual_selected_rows):
-    if derived_virtual_selected_rows is None:
-        derived_virtual_selected_rows = []
-
-    dff = df if rows is None else pd.DataFrame(rows)
-
-    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
-              for i in range(len(dff))]
-
-    return [
-        dcc.Graph(
-            id=column,
-            figure={
-                "data": [
-                    {
-                        "x": dff["country"],
-                        "y": dff[column],
-                        "type": "bar",
-                        "marker": {"color": colors},
-                    }
-                ],
-                "layout": {
-                    "xaxis": {"automargin": True},
-                    "yaxis": {
-                        "automargin": True,
-                        "title": {"text": column}
-                    },
-                    "margin": {"t": 30, "l": 10, "r": 10},
-                    
+    html.Div(
+        children=[
+            html.H4("Country Data", className="country-info"),
+            dash_table.DataTable(
+                id='global-data-table',
+                columns=[{"name": i, "id": i} for i in df.columns],
+                data=df.to_dict('records'),
+                filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                style_header={
+                    'fontWeight': 'bold'
                 },
-            },
-        )
-        # check if column exists - user may have deleted it
-        # If `column.deletable=False`, then you don't
-        # need to do this check.
-        for column in ["cases", "deaths", "recovered", "todayCases"]
-    ]
+                # Zebra style
+                style_data_conditional=[{
+                    'if': {'row_index': 'odd'},
+                    'backgroundColor': 'rgb(248, 248, 248)'
+                }],
+            )
+        ]
+    ), 
 
+    html.Div(
+        className="app-footer",
+        children=[
+            html.Div('Made with Dash'),
+        ]
+    ),
+
+    
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
