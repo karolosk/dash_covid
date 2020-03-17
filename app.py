474879a88b53f9ba2ba7fecc554b026c4ae9f3a1
“[firstname <karolos.koutsoulelos@gmail.com>
2020-03-17 20:36:05 +0200

    Several styling updates, app.py cleaned and data retriever refactor

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
-import pandas as pd
+from dash_table.Format import Format
 from datetime import date
-from data_retriever import get_all_data, create_data_file
+from data_retriever import get_all_data, get_data
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -16,28 +16,32 @@ app.title = 'dash-covid'
 server = app.server
 global_data = get_all_data()
 
-create_data_file()
-df = pd.read_csv('coronavirus_data_file_' + str(date.today()) + '.csv')
+df = get_data()
 
 app.layout = html.Div([
 
     html.Div(
         className="app-header",
         children=[
-            html.Div('Coronavirus Data', className="app-header--title"),
-            html.Div('Vizualization using Dash',  className="app-header--subtitle"),
+            html.P('Coronavirus Data', className="title"),
+            html.P('Visualization using Dash',  className="subtitle"),
         ]
     ),
 
     html.Div(
         children=[
+        
             html.Div(
                 className="total-info",
                 children=[
-                    html.H4("Global Data"),
-                    html.P("Total " + str(global_data[0][0]) + ": " + str(global_data[1][0])),
-                    html.P("Total " + str(global_data[0][1]) + ": " + str(global_data[1][1])),
-                    html.P("Total " + str(global_data[0][2]) + ": " + str(global_data[1][2])),
+                    html.P("TOTAL " + str(global_data[0][0]).upper(), className="total-info-label"),
+                    html.P(str(global_data[1][0]), className="total-info-data"),
+                    html.P("TOTAL " + str(global_data[0][1]).upper(), className="total-info-label"),
+                    html.P(str(global_data[1][1]), className="total-info-data"),
+                    html.P("TOTAL " + str(global_data[0][2]).upper(), className="total-info-label"),
+                    html.P(str(global_data[1][2]), className="total-info-data"),
+                    html.P("UPDATED AT", className="total-info-label"),
+                    html.P( global_data[2], className="total-info-date"),
                 ]
             ),
             
@@ -63,33 +67,51 @@ app.layout = html.Div([
 
     html.Div(
         children=[
-            html.H4("Country Data", className="country-info"),
             dash_table.DataTable(
                 id='global-data-table',
-                columns=[{"name": i, "id": i} for i in df.columns],
+                columns=[
+                        {"name": i, "id": i, 'type': 'numeric', 'format': Format(group=',')} for i in df.columns
+                    ],
                 data=df.to_dict('records'),
                 filter_action="native",
                 sort_action="native",
                 sort_mode="multi",
+                style_cell={'textAlign': 'left'},
                 style_header={
                     'fontWeight': 'bold'
                 },
-                # Zebra style
-                style_data_conditional=[{
-                    'if': {'row_index': 'odd'},
-                    'backgroundColor': 'rgb(248, 248, 248)'
-                }],
+                # Zebra style / conditional cell format
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'odd'},
+                        'backgroundColor': 'rgb(248, 248, 248)'
+                    },
+                    {
+                        'if': {'column_id': 'Country'},
+                        'fontWeight': 'bold'
+                    },
+                    {
+                        'if': {
+                            'column_id': 'Today Cases',
+                            'filter_query': '{Today Cases} != 0'
+                        },
+                        'backgroundColor': '#FFFBE5',
+                        'fontWeight': 'bold'
+                    },
+                    {
+                        'if': {
+                            'column_id': 'Today Deaths',
+                            'filter_query': '{Today Deaths} != 0'
+                        },
+                        'backgroundColor': '#FFE5E5',
+                        'color': '#B12305',
+                        'fontWeight': 'bold'
+                    },
+                ],
             )
-        ]
+        ],
+        className="data-table"
     ), 
-
-    html.Div(
-        className="app-footer",
-        children=[
-            html.Div('Made with Dash'),
-        ]
-    ),
-
     
 ])
 
--- data_retriever.py
+++ data_retriever.py
@@ -1,8 +1,9 @@
 import requests
 import json 
-from datetime import date
+from datetime import date,datetime
 import os.path
 import csv
+import pandas as pd
 
 def get_all_data():
     response = requests.get('https://corona.lmao.ninja/all')
@@ -11,14 +12,16 @@ def get_all_data():
 
     for key,value in json.loads(response.text).items():
         if key == 'updated':
+            updated_at = datetime.fromtimestamp(value/1000).strftime('%Y-%m-%d %H:%M:%S GMT+02:00')
+
             continue
+            
         keys.append(key)
         values.append(add_thousand_separator(value))
     
-    return[keys, values]
+    return[keys, values, updated_at]
 
-def add_thousand_separator(value):
-    return format(value, ',d')
+
 
 def create_data_file():
     filename = 'coronavirus_data_file_' + str(date.today()) + '.csv'
@@ -35,3 +38,35 @@ def create_data_file():
         dict_writer = csv.DictWriter(file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(response_to_list)
+
+
+def modify_data_frame():
+    df = pd.read_csv('coronavirus_data_file_' + str(date.today()) + '.csv')
+    
+    # Rename columns
+    df_new = rename_columns(df)
+    # Save to csv
+    df_new.to_csv('coronavirus_data_file_' + str(date.today()) + '.csv', index=False)
+
+def get_data():
+    create_data_file()
+    modify_data_frame()
+    return pd.read_csv('coronavirus_data_file_' + str(date.today()) + '.csv')
+
+
+def rename_columns(dataframe):
+    return dataframe.rename(columns={
+        'country': 'Country',
+        'cases': 'Cases',
+        'deaths': 'Deaths',
+        'todayCases': 'Today Cases',
+        'todayDeaths': 'Today Deaths',
+        'recovered': 'Recovered',
+        'critical': 'Critical'
+    })
+
+
+
+def add_thousand_separator(value):
+    return format(value, ',d')
+
