c7263790f9fa3b4e01f8e28a217a8fbe7f7e68bf
karolosk <karolos.koutsoulelos@gmail.com>
2020-09-18 09:53:36 +0300

    Minor error handing for corona api

--- service/data_service.py
+++ service/data_service.py
@@ -1,32 +1,41 @@
 import requests
-import json 
-from datetime import date,datetime
+import json
+from datetime import date, datetime
 import os.path
 import csv
 import pandas as pd
 
+
 def get_all_data():
-    
-    response = requests.get('https://corona.lmao.ninja/v2/all')
+
     keys = []
     values = []
-    
-    for key,value in json.loads(response.text).items():
+    try:
+        response = requests.get("https://corona.lmao.ninja/v2/all")
 
-        if key in ['cases', 'deaths', 'recovered', 'active']:
+        for key, value in json.loads(response.text).items():
 
-            keys.append(key)
-            values.append(add_thousand_separator(value))
+            if key in ["cases", "deaths", "recovered", "active"]:
 
-        elif key == 'updated':
+                keys.append(key)
+                values.append(add_thousand_separator(value))
 
-            updated_at = datetime.fromtimestamp(value/1000).strftime('%Y-%m-%d %H:%M:%S GMT')
-            continue
-            
-        else:
-            continue
-            # excluded keys : ['population', 'activePerOneMillion', 'continent','affectedCountries', 'casesPerOneMillion', 'testsPerOneMillion', 'tests', 'deathsPerOneMillion', 'todayDeaths', 'todayCases', 'critical']    
-    return[keys, values, updated_at]
+            elif key == "updated":
+
+                updated_at = datetime.fromtimestamp(value / 1000).strftime(
+                    "%Y-%m-%d %H:%M:%S GMT"
+                )
+                continue
+
+            else:
+                continue
+                # excluded keys : ['population', 'activePerOneMillion', 'continent','affectedCountries', 'casesPerOneMillion', 'testsPerOneMillion', 'tests', 'deathsPerOneMillion', 'todayDeaths', 'todayCases', 'critical']
+        return [keys, values, updated_at]
+    except Exception:
+        updated_at = datetime.fromtimestamp(value / 1000).strftime(
+            "%Y-%m-%d %H:%M:%S GMT"
+        )
+        return [["N/A"], ["/NA"], updated_at]
 
 
 def create_bar_chart_data():
@@ -35,83 +44,108 @@ def create_bar_chart_data():
     # df = pd.DataFrame.from_records(data[1:], columns=data[0])
     # print(df.head())
 
+
 def create_data_file():
-    filename = 'coronavirus_data_file_' + str(date.today()) + '.csv'
-    
+    filename = "coronavirus_data_file_" + str(date.today()) + ".csv"
+
     # Recheck
     # if os.path.isfile(filename):
     #     return
-    response = requests.get('https://corona.lmao.ninja/v2/countries')
+    response = requests.get("https://corona.lmao.ninja/v2/countries")
     # Check if we got valid response and if not then do nothing and just use previous file
-    if (response.status_code != 200):
+    if response.status_code != 200:
         return
-    
+
     response_to_list = json.loads(response.text)
-    
+
     # Check if we get empty response and then do nothing and just use previous file
-    if (len(response_to_list) == 0):
+    if len(response_to_list) == 0:
         return
 
     keys = response_to_list[0].keys()
-    # setting up encoding to avoid  UnicodeEncodeError 
+    # setting up encoding to avoid  UnicodeEncodeError
     # newline = ' to avoid having blank lines between lines
-    with open(filename, 'w', encoding="utf-8", newline='') as file:
+    with open(filename, "w", encoding="utf-8", newline="") as file:
         dict_writer = csv.DictWriter(file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(response_to_list)
 
 
 def modify_data_frame():
-    df = pd.read_csv('coronavirus_data_file_' + str(date.today()) + '.csv')
-    
+    df = pd.read_csv("coronavirus_data_file_" + str(date.today()) + ".csv")
+
     # Rename columns
     df_new = rename_columns(df)
 
     normalize_country_names(df_new)
     # Remove extra columns
-    df_new.drop(['active', 'countryInfo', 'deathsPerOneMillion'], axis=1, inplace=True)
+    df_new.drop(["active", "countryInfo", "deathsPerOneMillion"], axis=1, inplace=True)
 
-    df_new['Mortality Rate'] = round((df_new['Deaths']/df_new['Cases']) * 100, 2)
+    df_new["Mortality Rate"] = round((df_new["Deaths"] / df_new["Cases"]) * 100, 2)
 
     # Keeping only columns that we want to show in data table
-    df_final = df_new.filter(['Country', 'Cases', 'Deaths', 'Today Cases', 'Today Deaths', 'Recovered', 'Critical', 'Cases per million', 'Mortality Rate'])
+    df_final = df_new.filter(
+        [
+            "Country",
+            "Cases",
+            "Deaths",
+            "Today Cases",
+            "Today Deaths",
+            "Recovered",
+            "Critical",
+            "Cases per million",
+            "Mortality Rate",
+        ]
+    )
 
     # Sort by cases
-    df_final.sort_values(by=['Cases'], ascending=False, inplace=True)
+    df_final.sort_values(by=["Cases"], ascending=False, inplace=True)
 
     # Save to csv
-    df_final.to_csv('coronavirus_data_file_' + str(date.today()) + '.csv', index=False)
+    df_final.to_csv("coronavirus_data_file_" + str(date.today()) + ".csv", index=False)
+
 
 def get_data():
     create_data_file()
     modify_data_frame()
-    return pd.read_csv('coronavirus_data_file_' + str(date.today()) + '.csv')
+    return pd.read_csv("coronavirus_data_file_" + str(date.today()) + ".csv")
 
 
 def normalize_country_names(dataframe):
-    
-    dataframe['Country'].replace(['Congo, the Democratic Republic of the'], 'Congo', inplace=True)
-    dataframe['Country'].replace(['Tanzania, United Republic of'], 'Tanzania', inplace=True)
-    dataframe['Country'].replace(['Venezuela, Bolivarian Republic of'], 'Venezuela', inplace=True)
-    dataframe['Country'].replace(['Lao People\'s Democratic Republic'], 'Laos', inplace=True)
-    dataframe['Country'].replace(['Saint Vincent and the Grenadines'], 'St Vincent and the Grenadines', inplace=True)
 
-    
+    dataframe["Country"].replace(
+        ["Congo, the Democratic Republic of the"], "Congo", inplace=True
+    )
+    dataframe["Country"].replace(
+        ["Tanzania, United Republic of"], "Tanzania", inplace=True
+    )
+    dataframe["Country"].replace(
+        ["Venezuela, Bolivarian Republic of"], "Venezuela", inplace=True
+    )
+    dataframe["Country"].replace(
+        ["Lao People's Democratic Republic"], "Laos", inplace=True
+    )
+    dataframe["Country"].replace(
+        ["Saint Vincent and the Grenadines"],
+        "St Vincent and the Grenadines",
+        inplace=True,
+    )
+
+
 def rename_columns(dataframe):
-    return dataframe.rename(columns={
-        'country': 'Country',
-        'cases': 'Cases',
-        'deaths': 'Deaths',
-        'todayCases': 'Today Cases',
-        'todayDeaths': 'Today Deaths',
-        'recovered': 'Recovered',
-        'critical': 'Critical',
-        'casesPerOneMillion' : 'Cases per million'
-    })
-
+    return dataframe.rename(
+        columns={
+            "country": "Country",
+            "cases": "Cases",
+            "deaths": "Deaths",
+            "todayCases": "Today Cases",
+            "todayDeaths": "Today Deaths",
+            "recovered": "Recovered",
+            "critical": "Critical",
+            "casesPerOneMillion": "Cases per million",
+        }
+    )
 
 
 def add_thousand_separator(value):
-    return format(value, ',d')
-
-
+    return format(value, ",d")
